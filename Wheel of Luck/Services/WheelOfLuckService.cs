d8f604f657591cf2a535eb3c450531f14ad2f170
Vitalii Mamchur <146951542+vitaliipigoo@users.noreply.github.com>
2024-05-26 18:36:37 +0300

    WheelOfLuckService base implementation

--- Wheel of Luck/Services/WheelOfLuckService.cs
+++ Wheel of Luck/Services/WheelOfLuckService.cs
@@ -1,7 +1,45 @@
+using UnityEngine;
+using Wheel_of_Luck.AssetPackage.Scripts;
+using Wheel_of_Luck.Models;
+using Wheel_of_Luck.Scripts.Configurations;
+using Zenject;
+using Constants = Wheel_of_Luck.Scripts.Constants.Constants;
+
 namespace Wheel_of_Luck.Services
 {
-    public class WheelOfLuckService : IWheelOfLuckService
+    public class WheelOfLuckService : IWheelOfLuckService, IInitializable
     {
-        
+        private WheelOfLuckPopupAdapter _wofPopupAdapter;
+        private readonly WheelOfLuckConfiguration _wofConfiguration;
+
+        public WheelOfLuckService(WheelOfLuckConfiguration wofConfiguration)
+        {
+            _wofConfiguration = wofConfiguration;
+            
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            InitAdapter();
+        }
+
+        private void InitAdapter()
+        {
+            var prefab = Resources.Load<GameObject>(Constants.WheelOfLuckResourceName);
+            var instance = Object.Instantiate(prefab);
+
+            _wofPopupAdapter = new WheelOfLuckPopupAdapter(instance);
+        }
+
+        public void OpenWheelOfLuck(Transform canvas)
+        {
+            
+        }
+
+        public void OpenWheelOfLuck(WheelOfLuckConfigurationModel config, Transform canvas)
+        {
+            
+        }
     }
 }
